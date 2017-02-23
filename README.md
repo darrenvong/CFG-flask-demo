38f96438b8607c50292859e80c2cb768e7dd44a6
Darren Vong <d.vongy@gmail.com>
2017-02-23 23:48:28 +0000

    Simplified Flask demo back to the basics with extensive comments in the code

--- README.md
+++ README.md
@@ -3,25 +3,9 @@
 A collection of example Flask code written as demo in preparation for CF:G's Advanced Python course
 
 ## FAQs
-**In my Command Prompt/Terminal, sometimes I see the following message:
-`* Running on http://127.0.0.1:5000/`. So, why did you tell me to type `localhost:5000/[whatever_path_name]` instead in my browser?**
 
-This is because `localhost` is "the same" as `127.0.0.1` - essentially, it is an easier, more human-readable version of the web address
-pointing to the server you are running locally.
-
-**Why do all of your template files end in `.htm.j2` rather than `.html`?**
-
-This is purely done to make sure my template files are highlighted correctly with pretty and helpful colours in my text editor.
-You may use either the `htm.j2` or `.html` file ending. As long you are referring to the templates by the same name when using
-the `render_template` function, everything should work as expected.
-
-For example, if you have a template called `hello.htm.j2` in the `templates` folder, write
-```python
-render_template("hello.htm.j2")
-```
-
-**Why is it that in the `hello` function, it reads `render_template("hello.htm.j2")`, whereas in the `hello_someone` function,
-it reads `render_template("hello.htm.j2", name=name.title())` ?**
+**Why is it that in the `hello` function, it reads `render_template("hello.html")`, whereas in the `hello_someone` function,
+it reads `render_template("hello.html", name=name.title())` ?**
 
 The `render_template` function accepts an unlimited number of **optional** named variables after the name of the template, which when passed to the `render_template` function, the data/value in the named variables themselves become accessible in the template files. This
 is important as it allows **dynamic** data to be displayed without the need to write separate HTML templates for every possible name the user may type in through the browser.
--- demo.py
+++ demo.py
@@ -1,42 +1,67 @@
+# To use functions from a library you installed via pip, you have to import them first.
+# Importing functions using the "from x import a, b, c" style allows you to use
+# the function directly as if *you* had written the code yourself.
 from flask import Flask, render_template, request
 
+# This creates the all important Flask application which will give us the ability
+# to serve (respond) web pages back to our users based on the web address they
+# typed in the browser
 app = Flask("demo")
 
-@app.errorhandler(400)
-def display_400(error):
-    return render_template("400.htm.j2")
+"""The @app.route(...) lines before the following functions are what's known as
+"decorators". The basic concept you need to know here is that they "decorate"
+the standard functions so that they can respond to users who will be using
+the application through a browser."""
 
+# This will return the sentence "Hello Code First Girls" to the user's browser who
+# visit the root path of your website. (most likely "localhost" since you will be
+# developing this application locally on your computer first)
 @app.route("/")
 def hello():
-    return render_template("hello.htm.j2")
+    return "Hello Code First Girls"
 
+# <name> is a placeholder which holds the value of whatever a user has typed in
+# after "localhost". The placeholder is what gives the flexibility that allows this
+# hello string to address to someone accordingly depending on what they typed in
+# the address in their browser
 @app.route("/<name>")
 def hello_someone(name):
-    return render_template("hello.htm.j2", name=name.title())
+    return "Hello {0}!".format(name)
 
+# For more complicated applications, it is easier to display pages through HTML,
+# a language which is used to layout the pages you see by your browser. Flask has
+# added another layer on top which allows your application to feed dynamic contents
+# to your HTML through the use of "templates"
 @app.route("/bye/<name>")
 def bye_someone(name):
-    return render_template("hello.htm.j2", bye=True, name=name.title())
+    # In order for the templates we are using to have access to the dynamic data
+    # processed in Python/Flask, we need to pass these as additional variables
+    # after the name of the template. In this example, the name the user has
+    # typed in the web address will be made available to the template via the
+    # variable called "name"
+    return render_template("hello.html", bye=True, name=name.title())
 
 @app.route("/goodbye/<name>/<time>")
 def bye_custom(name, time):
-    return render_template("hello.htm.j2", bye=True, name=name.title(), time=time)
+    return render_template("hello.html", bye=True, name=name.title(), time=time)
 
+
+# Normally, when you are typing in a web address to visit a website, you are doing
+# so under a method known as a "get request". However, when you need to give users
+# the ability to send data through a form (e.g. when you are sending in your name
+# and password to log in to the university's website), this is done via a different
+# "method" known as a "post request".
+#
+# In order for your functions to be able to respond to a "post request", in addition
+# to a normal "get request", you need to declare it after the name of the path
+# you want your page and your form to respond to in app.route(...) as shown below
 @app.route("/signup", methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'GET':
-        return render_template("feedback.htm.j2")
-    else:
-        return render_template("ok.htm.j2", form_data=request.form)
+    if request.method == 'GET': # the request comes from a normal browser visit to the website
+        return render_template("feedback.html")
+    else: # the request is send from a form you have set up in the HTML of your page
+        return render_template("form_response.html", form_data=request.form)
 
-# A Pythonic convention which allows you import this script without necessarily
-# running the flask app defined here
-if __name__ == "__main__":
-    
-    """In class, to see the results of the application, we had to type
-    'localhost:5000/[path_name]' to see the returned result in our browser.
-    the extra 'port=80' saves you some keystroke and allows you to just type
-    'localhost/[path_name]' as by default, 'localhost' is equivalent to
-    'localhost:80' in browsers.
-    """
-    app.run(port=80, debug=True)
+# Passing in "debug=True" to app.run(...) will make your Flask powered server/
+# application automatically refreshes upon any changes you make to this file.
+app.run(debug=True)
